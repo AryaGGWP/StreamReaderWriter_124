218e5bfbb5f7fc6dceffafe2fac6d3300103b43b
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-06-19 11:20:17 +0700

    default exception

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -12,4 +12,10 @@ int main()
 		//block ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
 	}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+
+	return;
 }
